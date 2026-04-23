92cb5b4778cd6249f768db16d9c204c9339db974
Willjianger9 <1jiangwil@gmail.com>
2026-04-23 16:35:34 -0700

    implemented consumer section

---
+++ tech_lab_on_campus/market_watch/producer_and_consumer/consumer/solution/.ipynb_checkpoints/consumer_sol-checkpoint.py
@@ -0,0 +1,56 @@
+import pika
+import os
+from consumer_interface import mqConsumerInterface
+
+class mqConsumer(mqConsumerInterface):
+    def __init__(
+        self, binding_key: str, exchange_name: str, queue_name: str) -> None:
+        self.binding_key = binding_key
+        self.exchange_name = exchange_name
+        self.queue_name = queue_name
+        self.setupRMQConnection()
+
+    def setupRMQConnection(self) -> None:
+        con_params = pika.URLParameters(os.environ["AMQP_URL"])
+        self.connection = pika.BlockingConnection(parameters=con_params)
+
+        self.channel = self.connection.channel()
+
+        self.channel.queue_declare(queue=self.queue_name)
+
+        self.channel.exchange_declare(exchange=self.exchange_name)
+
+        self.channel.queue_bind(
+            queue=self.queue_name,
+            routing_key=self.binding_key,
+            exchange=self.exchange_name,
+        )
+
+        self.channel.basic_consume(
+            self.queue_name, self.on_message_callback, auto_ack=False
+        )
+
+    def on_message_callback(
+        self, channel, method_frame, header_frame, body
+    ) -> None:
+        channel.basic_ack(method_frame.delivery_tag, False)
+
+        print(f" [x] Received Message: {body.decode('utf-8')}")
+
+    def startConsuming(self) -> None:
+        print("Waiting for messages...")
+
+        self.channel.start_consuming()
+    
+    def __del__(self) -> None:
+        print("Closing RMQ connection on destruction")
+        
+        try:
+            self.channel.close()
+        except Exception:
+            pass
+
+        try:
+            self.connection.close()
+        except Exception:
+            pass
